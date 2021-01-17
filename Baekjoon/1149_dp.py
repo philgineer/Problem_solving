b6769d7d864e262753634d3655e595c9497b9932
Junho Yun <43261253+philgineer@users.noreply.github.com>
2021-01-18 02:16:18 +0900

    solved!

--- Baekjoon/1149_dp.py
+++ Baekjoon/1149_dp.py
@@ -1,8 +1,22 @@
-arr = [[0]*8]*8
+import sys
+sys.setrecursionlimit(10000)
 
-arr[1][1] = 9
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [(0, 0, 0) for _ in range(n + 1)]
 
-print('[')
-for row in arr:
-    print(row)
-print(']')
+def get_min_sofar(k):
+    if dp[k] != (0, 0, 0):
+        return dp[k]
+    r, g, b = arr[k - 1]
+    if k == 1:
+        dp[1] = r, g, b
+        return r, g, b
+    pre_r, pre_g, pre_b = get_min_sofar(k - 1)
+    curr_r = min(pre_g, pre_b) + r
+    curr_g = min(pre_r, pre_b) + g
+    curr_b = min(pre_r, pre_g) + b
+    dp[k] = curr_r, curr_g, curr_b
+    return curr_r, curr_g, curr_b
+
+print(min(get_min_sofar(n)))
